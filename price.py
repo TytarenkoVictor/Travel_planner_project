619191e70a32c19f32cb5298a7e1b8d7bb74570d
TytarenkoVictor <44294074+TytarenkoVictor@users.noreply.github.com>
2019-05-18 17:09:33 +0300

    Update price.py

--- price.py
+++ price.py
@@ -9,7 +9,9 @@ class CostOfLiving:
 
     def __init__(self, city):
         """This method initializes."""
-        self.city = city
+        self.city = city.replace(" ", "-")
+        if self.city == "Krakow":
+            self.city = "Krakow-Cracow"
         self.table = None
 
     def parse(self):
@@ -23,94 +25,50 @@ class CostOfLiving:
 
     def extract_data(self):
         """This method extract data and creates list with it."""
+        self.parse()
         lst = []
         for i in self.table.text.split("\n")[3:]:
             if i != "" and bool(re.search(r'\d', i)):
                 lst.append(i.replace(u'\xa0', ''))
         single = lst.pop(-3)
-        lst = [i+" "+j for i, j in zip(lst[::2], lst[1::2])]
+        lst = [i + " " + j for i, j in zip(lst[::2], lst[1::2])]
         lst.append(single)
-        return lst
+        return lst[0:28]
 
-    def estimate_food_expences(self, days):
-        """Recommended minimum amount of money for food
-        (2400 calories, Western food types)"""
+    def estimate_food_expences(self, hours):
+        food = self.extract_data()
+        """This method estimate food expences in given city during given period
+        of time(in hours). Recommended minimum amount of money for food + water
+        (2400 calories per day, Western food types). Milk - 0.25 liter,
+        Bread - 125 g, Rice - 0.1 kg, Eggs - 2.4, Cheese - 0.1 kg,
+        Chicken - 0.15 kg, Beef - 0.15 kg, Apples - 0.3 kg, Banana - 0.25 kg,
+        Oranges - 0.3 kg, Tomato - 0.2 kg, Potato - 0.2 kg, Onion - 0.1 kg,
+        Lettuce - 0.2 head, Water - 1.5 litter."""
         portions_per_day = [0.25, 0.25, 0.10, 0.2, 0.10, 0.15,
         0.15, 0.30, 0.25, 0.30, 0.20, 0.20, 0.10, 0.20, 1]
         total = 0
-        for i, e in enumerate(self.extract_data()[8:23]):
+        for i, e in enumerate(food[8:-5]):
             total += float(e.split()[-2][0:-1]) * portions_per_day[i]
-        home = round(total, 2) * days
-        inexpensive = float(self.extract_data()[0].split()[-2][0:-1]) * 3 * days
-        mid = float(self.extract_data()[1].split()[-2][0:-1]) * 1.5 * days
-        mac = float(self.extract_data()[2].split()[-2][0:-1]) * 3 * days
+        home = round(round(total, 2) * round(hours/24, 2), 2)
+        inexpensive = round(float(food[0].split()[-2][0:-1]) * \
+        round(hours/8, 2), 2)
+        mid = round(float(food[1].split()[-2][0:-1]) * round(hours/16, 2), 2)
+        mac = round(float(food[2].split()[-2][0:-1]) * round(hours/8, 2), 2)
         return home, mac, inexpensive, mid
 
-    def estimate_transport_expences(self, days):
-        """Recommended minimum amount of money for transport
-        (3 trips per day, public transport)"""
-        transport = float(self.extract_data()[27].split()[-2][0:-1]) * 3 * days
+    def additional(self):
+        """This method returns detail info about food + water prices
+        in given city."""
+        data = self.extract_data()
+        st = ""
+        for i in data[8:-5]:
+            st += i + "\n"
+        return st
+
+    def estimate_transport_expences(self, hours):
+        """This method estimate transport expences in given city during given
+        period of time(in hours). Recommended minimum amount of money for
+        transport (3 trips per day, public transport)"""
+        pr = self.extract_data()[27]
+        transport = round(float(pr.split()[-2][0:-1]) * round(hours/8, 2), 2)
         return transport
-
-
-if __name__ == "__main__":
-    city = "Lisbon"
-    c = CostOfLiving(city)
-    c.parse()
-    c.extract_data()
-    print(c.estimate_food_expences(3))
-    print(c.estimate_transport_expences(3))
-
-
-
-
-
-
-
-
-
-
-# site = "https://www.numbeo.com/cost-of-living/in/Lviv?displayCurrency=EUR"
-#
-# # site = "https://www.numbeo.com/cost-of-living/compare_cities.jsp?country1=Ukraine&country2=Ukraine&city1=Kiev&city2=Lviv&tracking=getDispatchComparison"
-#
-# hdr = {'User-Agent': 'Mozilla/5.0'}
-# req = Request(site, headers=hdr)
-# page = urlopen(req)
-# soup = BeautifulSoup(page, "html.parser")
-# # summary = soup.find("div", attrs={"class": "seeding-call limit_size_ad_right padding_lower other_highlight_color"})
-# # print(summary.text)
-# # di = soup.find("div", attrs={"class": "column mod-main"})
-# table = soup.find("table", attrs={"class": "data_wide_table"})
-# print(table.text)
-
-
-
-
-# def cost_of_living(city, comp):
-#     if comp == "-":
-#         site = "https://www.numbeo.com/cost-of-living/in/{}?displayCurrency=EUR".format(city)
-#     else:
-#         site = "https://www.numbeo.com/cost-of-living/compare_cities.jsp?\
-#         &city1={}&city2={}&\
-#         tracking=getDispatchComparison".format(comp, city)
-#
-#     hdr = {'User-Agent': 'Mozilla/5.0'}
-#     req = Request(site, headers=hdr)
-#     page = urlopen(req)
-#     soup = BeautifulSoup(page, "html.parser")
-#     if comp == "-":
-#         summary = soup.find("div", attrs={"class": "seeding-call limit_size_ad_right padding_lower other_highlight_color"})
-#         table = soup.find("table", attrs={"class": "data_wide_table"})
-#         return summary.text, table.text
-    # else:
-
-
-# if __name__ == "__main__":
-#     user1 = input("Enter city :")
-#     compare = input("Do you want to caompare with other city?(+/-)")
-#     if compare == "+":
-#         comp = input("Enter city, which you want compare to :")
-#     elif compare == "-":
-#         comp = "-"
-#     print(cost_of_living(user1, comp))
