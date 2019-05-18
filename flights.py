7db443671b8c18c7482a9815cfbddcd2f9d835f8
TytarenkoVictor <44294074+TytarenkoVictor@users.noreply.github.com>
2019-05-18 17:10:16 +0300

    Update flights.py

--- flights.py
+++ flights.py
@@ -1,101 +1,47 @@
 from urllib import request
-from linkedqueue import LinkedQueue
+from datetime import datetime
 import json
 
 
 class Search:
-    """ """
-    URL = "https://api.skypicker.com/flights?flyFrom={}&to={}&dateFrom={}&partner=picky"
+    """This class used for searching flights from given city to final
+    and back."""
+    URL = "https://api.skypicker.com/flights?flyFrom\
+={}&to={}&dateFrom={}&dateTo={}&partner=picky"
 
-    def __init__(self, u_from, u_to, d_from):
-        """ """
-        self.u_from = u_from
-        self.u_to = u_to
+    def __init__(self, u_from, u_to, d_from, d_to):
+        """This method initializes."""
+        self.u_from = u_from.replace(" ", "-")
+        self.u_to = u_to.replace(" ", "-")
         self.d_from = d_from
+        self.d_to = d_to
 
     def url_request(self):
-        """ """
-        url_param = Search.URL.format(self.u_from, self.u_to, self.d_from)
+        """This method gets data from kiwi.com."""
+        url_param = Search.URL.\
+        format(self.u_from, self.u_to, self.d_from, self.d_to)
         url1 = request.urlopen(url_param)
-        content = url1.read().decode(url1.info().get_param('charset') or 'utf-8')
+        content = url1.read().decode(url1.info().\
+        get_param('charset') or 'utf-8')
         data = json.loads(content)
         return data
 
     def data_extract(self):
-        """ """
-        flights = LinkedQueue()
-        for i in self.url_request()['data']:
+        """This method extract data about flight: date, price, duration,
+        link ex."""
+        flight = []
+        m = self.url_request()['data'][0]
+        flight.append(m['price'])
+        flight.append(m['fly_duration'])
+        flight.append(len(m['route']))
+        link = m['deep_link']
+        full_route = []
+        for i in m['route']:
             lst = []
-            lst.append(i['price'])
-            lst.append(i['fly_duration'])
-            lst.append(len(i['route']))
-            flights.push(lst)
-
-
-if __name__ == "__main__":
-    u_from = "kiev"
-    u_to = "paris"
-    d_from = "15/08/2019"
-    d_to = "18/08/2019"
-    # u_from = input("from :")
-    # u_to = input("to :")
-    # d_from = input("date from :")
-    # d_to = input("date to :")
-    r = Search(u_from, u_to, d_from)
-    print(r.data_extract())
-    r = Search(u_to, u_from, d_to)
-    print(r.data_extract())
-
-
-
-
-
-# r.url_request()['data'][0]
-# id-
-# countryFrom
-# countryTo
-# bags_price
-# baglimit
-# dTime
-# aTime
-# dTimeUTC
-# p1
-# p2
-# p3
-# aTimeUTC
-# price+
-# flyFrom
-# mapIdfrom
-# mapIdto
-# flyTo
-# distance
-# cityFrom+
-# cityTo+
-# route
-# routes
-# airlines
-# nightsInDest
-# pnr_count
-# transfers
-# has_airport_change
-# fly_duration+
-# duration
-# facilitated_booking_available
-# type_flights
-# found_on
-# conversion
-# booking_token
-# quality
-# deep_link+
-# search_id+
-# data!!!!!!!!
-# connections-
-# time+1
-# currency+
-# currency_rate+1
-# refresh-
-# del+??
-# ref_tasks-
-# search_params
-# all_stopover_airports-
-# all_airlines-
+            lst.append(str(datetime.fromtimestamp(i['aTimeUTC'])))
+            lst.append(str(datetime.fromtimestamp(i['dTimeUTC'])))
+            lst.append(i['cityFrom'].replace("ó", "o"))
+            lst.append(i['cityTo'].replace("ó", "o"))
+            full_route.append(lst)
+        flight.append(full_route)
+        return flight, link
